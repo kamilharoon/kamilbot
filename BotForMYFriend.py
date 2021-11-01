1091ccc856a4bb28a7e17788dcf143c241bec79e
kamilharoon <kamilharoon963@gmail.com>
2021-11-01 20:20:42 +0530

    Create BotForMYFriend.py

---
+++ BotForMYFriend.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+
+prefix = "!"
+bot = commands.Bot(command_prefix=prefix)
+bot.remove_command("help")
+
+@bot.event
+async def on_ready():
+    bot.loop.create_task(status_task())
+    print("Started successfully")
+
+async def status_task():
+    await bot.change_presence(activity=discord.Game("online"),
+                              status=discord.Status.online)
+
+
+@bot.command()
+async def hello(ctx, member: discord.Member):
+    await ctx.send(f"{ctx.author.name} sends you an hello {member}")
+@bot.command()
+async def help(ctx):
+    await ctx.send(f"Commands: hello [user]")
+    @bot.command()
+    async def hi(ctx):
+        await ctx.send(f"Commands:Welcome @shaheenkk#6300")
+
+bot.run("ODUwNjc2Mjc3MzMyNTQxNDUx.YLtMBQ.bL0WNScZ9405GvShST1KfNuxqnc")
+
+
+
